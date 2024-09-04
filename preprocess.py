3d2fe24d4e8cb6eac85a7817231b5fdb2db54703
Smogshaik <65766894+CarlosHartmann@users.noreply.github.com>
2024-09-04 22:47:05 +0200

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,7 +1,7 @@
 #!/usr/local/bin/python3
 
 '''
-preprocess: Für neue MKVs für Filmsammlung; stellt sicher, dass Metadaten lesbar sind löscht unerwünschten Filmtitel, erstellt plexfreundliche Tonspuren.
+preprocess: Ensures that metadata are readable, deletes undesired noisy metadata, and creates easily-streamable downmixed AAC alternative audio tracks
 '''
 
 import os
@@ -18,10 +18,16 @@ from check_valid_default import *
 from fix_subs_audio import *
 from fix_subs_audio import inspect_mkv as subfix
 
+# The output paths
 goal_path = "/Users/Carlitos/Movies/premiumize_downloads/out/originale_out"
-goal_4k = "/Users/Carlitos/Movies/premiumize_downloads/out/4K_out"
+goal_4k = "/Users/Carlitos/Movies/premiumize_downloads/out/4K_out" # I am not sure anymore if I ended up using this
 
 def check_metadata(mkvinfo, file):
+	'''
+	Usually, in MKV files, metadata come first in the bytestream and are thus readable by libraries like mkvinfo.
+ 	In rare cases, there are other things at the beginning of the bytestream, rendering the metadata unreadable.
+  	mkvinfo was at the time of writing not great at noticing incomplete metadata, so this function checks for us.
+ 	'''
 	print("Checking metadata...")
 	lines = iter(mkvinfo.split('\n'))
 	found = False
@@ -43,13 +49,14 @@ def preprocess(file, goal_path, root="/Users/Carlitos/Movies/premiumize_download
 		print("Fixing the subtitles:")
 		subfix(file, root)
 		print("Normal audio conversion:")
-		conv = normal_convert(file, sys.stdout)
-		if conv != 0 and conv != 'leave as is':
+		conv = normal_convert(file, sys.stdout) # this launches a conversion process and is 0 when successful
+		if conv != 0 and conv != 'leave as is': # when not successful, a different workaround script often did the trick -- though I forgot what the workaround does differently
 			print("Workaraound was necessary:")
 			conv = workaround_convert(file, sys.stdout)
 			if conv != 0:
-				print("Both conversion attempts failed.")
+				print("Both conversion attempts failed.") # never had this happen, thankfully
 				raise Exception
+		# Some files already satisfy my goal profile because they only contain Stereo audio tracks, for example
 		elif conv == 'leave as is':
 			new_name = file
 		elif conv == 0:
@@ -63,20 +70,20 @@ def preprocess(file, goal_path, root="/Users/Carlitos/Movies/premiumize_download
 
 def main():
 	global goal_path
-	if len(sys.argv) == 1:
+	if len(sys.argv) == 1: # defaults to this path if no other is defined in the calling of the script
 		arg = "/Users/Carlitos/Movies/premiumize_downloads/out/originale_in"
 	else:
 		arg = sys.argv[1]
 	if os.path.isdir(arg):
 		for root, dirs, files in os.walk(arg):
 			for file in files:
-				if "4K" in root:
+				if "4K" in root: # probably not necessary
 					goal_path = goal_4k
-				if not os.path.isdir(goal_path):
+				if not os.path.isdir(goal_path): # create path if it doesn't exist yet
 					subprocess.call(['mkdir', '-p', goal_path])
 				goal_file = os.path.join(goal_path, file)
 				file = os.path.join(root, file)
-				if not os.path.isfile(goal_file) and file.endswith(".mkv") and not file.endswith("-new.mkv") and not file.endswith("-temp.mkv"):
+				if not os.path.isfile(goal_file) and file.endswith(".mkv") and not file.endswith("-new.mkv") and not file.endswith("-temp.mkv"): # don't accidentally process files that were created by my own script
 					print("\n\n\nStarting", file)
 					preprocess(file, goal_path, root)
 				else:
@@ -86,5 +93,6 @@ def main():
 		preprocess(arg, goal_path)
 
 if __name__ == "__main__":
+	# I used some file for most of the logs in case the output was unexpected. The script runs fine nowadays so I haven't used this in a long while. You may skip this.
 	sys.stdout=open("/Users/Carlitos/Library/CloudStorage/Dropbox/05 – Technisches/Data Storage/software_filmsammlung/log_preprocess_hotfolder.txt", "a")
 	main()
